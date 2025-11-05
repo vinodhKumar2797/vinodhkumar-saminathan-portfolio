a6bac06c7cc9a51622a37b888482113812e708aa
vinodhKumar2797 <vinothkumars395@gmail.com>
2025-11-04 18:14:41 -0600

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    base:"vinodhkumar-saminathan-portfolio",
+    base:"/vinodhkumar-saminathan-portfolio/",
     host: "::",
     port: 8080,
   },
